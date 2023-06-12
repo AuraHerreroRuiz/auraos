877449a98cb13691b9ec11935929c7b532ff8d50
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-06-12 16:16:51 +0200

    fix: missing quotes in urls in build-kup.sh

--- scripts/build-kup.sh
+++ scripts/build-kup.sh
@@ -12,7 +12,7 @@ echo "Building Bup"
 #Build bup
 mkdir /tmp/bupbuild
 cd /tmp/bupbuild
-git clone https://github.com/bup/bup.git
+git clone 'https://github.com/bup/bup.git'
 git checkout 0.33
 cd bup
 make long-check
@@ -23,7 +23,7 @@ cd /tmp/kupbuild
 echo "Installing Kup dependencies" 
 dnf -y install cmake extra-cmake-modules qt5-qtbase qt5-qtbase-devel kf5-kcoreaddons kf5-kcoreaddons-devel kf5-kdbusaddons kf5-kdbusaddons-devel kf5-ki18n kf5-ki18n-devel kf5-kio kf5-kio-devel kf5-solid kf5-solid-devel kf5-kidletime kf5-kidletime-devel kf5-knotifications kf5-knotifications-devel kf5-kconfig kf5-kconfig-devel kf5-kjobwidgets kf5-kjobwidgets-devel kf5-plasma kf5-plasma-devel libgit2 libgit2-devel
 echo "Building Kup"
-git clone https://invent.kde.org/system/kup.git
+git clone 'https://invent.kde.org/system/kup.git'
 cd kup
 mkdir build
 cd build
