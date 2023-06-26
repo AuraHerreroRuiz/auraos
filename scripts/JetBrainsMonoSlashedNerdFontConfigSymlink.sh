ae4bf327ab398e4e11b9001403b87179ebcdafeb
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-06-26 03:08:34 +0200

    fix: not re-generating font cache

--- scripts/JetBrainsMonoSlashedNerdFontConfigSymlink.sh
+++ scripts/JetBrainsMonoSlashedNerdFontConfigSymlink.sh
@@ -2,3 +2,4 @@
 set -oue pipefail
 mkdir -p "/usr/etc/fonts/conf.d/"
 ln -s "/usr/share/fontconfig/conf.avail/60-jetbrains-mono-fonts.conf" "/usr/etc/fonts/conf.d/60-jetbrains-mono-fonts.conf"
+fc-cache -fv
