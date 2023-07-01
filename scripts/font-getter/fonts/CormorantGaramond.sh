d9764274fedd60c6bd63ff5b009f3476b7079b06
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-07-01 14:43:44 +0200

    fix: duplicated file extension

--- scripts/font-getter/fonts/CormorantGaramond.sh
+++ scripts/font-getter/fonts/CormorantGaramond.sh
@@ -9,7 +9,7 @@ curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/Cormoran
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-Italic.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-Italic.ttf
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-Medium.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-Medium.ttf
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-MediumItalic.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-MediumItalic.ttf
-curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-SemiBold.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-SemiBold.ttf.ttf
+curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-SemiBold.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-SemiBold.ttf
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-SemiBoldItalic.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-SemiBoldItalic.ttf
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-Bold.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-Bold.ttf
 curl -GL https://github.com/google/fonts/raw/main/ofl/cormorantgaramond/CormorantGaramond-BoldItalic.ttf -o /tmp/usr/share/fonts/cormorant-garamond/CormorantGaramond-BoldItalic.ttf
