bd1aab23e0e830e72351c2f78d6fd2b0db9b2d0f
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-09-14 23:21:44 +0200

    fix: shell env variable not set correctly

--- usr/etc/profile.d/auraos-envs.sh
+++ usr/etc/profile.d/auraos-envs.sh
@@ -3,6 +3,5 @@ export XCURSOR_THEME=Posys_cursor_Linux_black
 export VISUAL=micro
 export EDITOR=micro
 export PAGER=moar
-export SHELL=/usr/bin/zsh
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 export GTK_USE_PORTAL=1
