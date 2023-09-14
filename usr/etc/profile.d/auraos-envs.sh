ad616f8ebd69a6e83bf029ead7d5130481c44a67
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-09-14 15:56:43 +0200

    fix: env variables not applying correctly

---
+++ usr/etc/profile.d/auraos-envs.sh
@@ -0,0 +1,8 @@
+export XCURSOR_SIZE=36
+export XCURSOR_THEME=Posys_cursor_Linux_black
+export VISUAL=micro
+export EDITOR=micro
+export PAGER=moar
+export SHELL=/usr/bin/zsh
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+export GTK_USE_PORTAL=1
