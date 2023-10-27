8238daaaf76868d32d843868f65b393ba54d96fc
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-10-27 10:10:21 +0200

    fix: nonlocal replacement override not implemented

--- config/scripts/replace-spectacle-regression.sh
+++ config/scripts/replace-spectacle-regression.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 # Avoid regression https://bugs.kde.org/show_bug.cgi?id=473879
-rpm-ostree override replace 'https://koji.fedoraproject.org/koji/buildinfo?buildID=2258866'
+
+curl 'https://kojipkgs.fedoraproject.org//packages/spectacle/23.04.3/2.fc38/x86_64/spectacle-23.04.3-2.fc38.x86_64.rpm' --output /tmp/spectacle.rpm
+rpm-ostree override replace /tmp/spectacle.rpm
