575f30675695fe9f0fd80b128bf060fc780e61d6
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-11-29 02:41:16 +0100

    fix: restore p10k os_icon

--- config/files/usr/etc/p10k/p10k.zsh
+++ config/files/usr/etc/p10k/p10k.zsh
@@ -178,8 +178,8 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=160
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=240
   # Custom icon.
   #typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
 
