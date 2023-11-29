f9afcbd14f04d452f8ff742ff61f9328e561a3fa
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2023-11-29 17:23:02 +0100

    fix: better fedora p10k colour

--- config/files/usr/etc/p10k/p10k-portable.zsh
+++ config/files/usr/etc/p10k/p10k-portable.zsh
@@ -181,7 +181,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=4
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=12
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
