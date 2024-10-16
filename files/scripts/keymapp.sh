69ff72f0f2a387a375a8809fee4f9ee13eb44c8f
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2024-10-16 16:25:49 +0200

    fix: bin directory missing

--- files/scripts/keymapp.sh
+++ files/scripts/keymapp.sh
@@ -6,5 +6,6 @@ mkdir -p /tmp/keymapp-download
 # From https://www.zsa.io/flash
 wget https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz -O /tmp/keymapp-download/keymapp.tar.gz
 # Extract binary
-tar -xf /tmp/keymapp-download/keymapp.tar.gz keymapp --directory /usr/bin
+mkdir -p /usr/bin
+tar -xf /tmp/keymapp-download/keymapp.tar.gz keymapp --group root --owner root --directory /usr/bin
 chmod 755 /usr/bin/keymapp
