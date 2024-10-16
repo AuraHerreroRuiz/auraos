ab806e8942bca9a12360d59cc71dd952c90fc333
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2024-10-16 15:25:41 +0200

    feat: keymapp

---
+++ files/scripts/keymapp.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Download binary
+set -oue pipefail
+mkdir -p /tmp/keymapp-download
+# From https://www.zsa.io/flash
+wget https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz -O /tmp/keymapp-download/keymapp.tar.gz
+# Extract binary
+tar -xf /tmp/keymapp-download/keymapp.tar.gz keymapp --directory /usr/bin/keymapp
+chmod 755 /usr/bin/keymapp
