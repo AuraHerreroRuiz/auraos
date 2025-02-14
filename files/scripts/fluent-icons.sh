0b689fc33a5219599b1a7aa14956e2910abdc544
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2025-02-14 23:38:44 +0100

    fix: move Fluent Icons from main to last release

--- files/scripts/fluent-icons.sh
+++ files/scripts/fluent-icons.sh
@@ -6,7 +6,7 @@ set -oe pipefail
 
 cd /tmp
 git clone https://github.com/vinceliuice/Fluent-icon-theme
-
+git checkout "$(git tag --sort=taggerdate --list '[0-9]*' | tail -n 1)"
 #!/usr/bin/env bash
 
 # if [ ${UID} -eq 0 ]; then
