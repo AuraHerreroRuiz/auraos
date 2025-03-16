33d8d14df0e115fcd48c4c9fb876ea7f5b056c53
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2025-03-16 20:09:44 +0100

    fix: no need for wget, use curl

--- files/scripts/zshplugins.sh
+++ files/scripts/zshplugins.sh
@@ -5,15 +5,15 @@ set -oue pipefail
 #Use this???
 mkdir -p /usr/share/zsh-history-substring-search
 cd /usr/share/zsh-history-substring-search
-wget https://raw.githubusercontent.com/zsh-users/zsh-history-substring-search/master/zsh-history-substring-search.zsh
+curl -O https://raw.githubusercontent.com/zsh-users/zsh-history-substring-search/master/zsh-history-substring-search.zsh
 
 mkdir -p /usr/share/zsh-interactive-cd
 cd /usr/share/zsh-interactive-cd
-wget https://raw.githubusercontent.com/mrjohannchang/zsh-interactive-cd/master/zsh-interactive-cd.plugin.zsh
+curl -O https://raw.githubusercontent.com/mrjohannchang/zsh-interactive-cd/master/zsh-interactive-cd.plugin.zsh
 
 mkdir -p /usr/share/zsh-sudo
 cd /usr/share/zsh-sudo
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
 
 git clone https://github.com/b4b4r07/enhancd.git /usr/share/enhancd
 
