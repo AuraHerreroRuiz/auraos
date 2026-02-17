7e990263b1a967f7e0d49ea0d77792e3a84d02cb
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2026-02-17 18:36:16 +0100

    fix: checksum of new kdotool version

--- files/scripts/kdotool.sh
+++ files/scripts/kdotool.sh
@@ -7,6 +7,6 @@ wget https://github.com/jinliu/kdotool/releases/download/v0.2.2/kdotool-0.2.2-x8
 mkdir -p /usr/bin
 cd /usr/bin
 tar -xf /tmp/kdotool-download/kdotool.tar.gz kdotool --group root --owner root
-echo "85b04d6dede715c217c29c72bb0f81fc041e405cf9ea4dece859443a2bfd88b033626e66cb3337ba1696051b78fe2caa41ad90c86ac323f9175d0d8753f0631a  kdotool" | sha256sum -c
+echo "85b04d6dede715c217c29c72bb0f81fc041e405cf9ea4dece859443a2bfd88b033626e66cb3337ba1696051b78fe2caa41ad90c86ac323f9175d0d8753f0631a  kdotool" | sha512sum -c
 chmod 755 /usr/bin/kdotool
 
